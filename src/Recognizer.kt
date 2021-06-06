ceef18d3ee22d4215ca1ece503c2256223a3653b
Simon Huang <simon.huang@tum.de>
2021-06-06 20:04:31 +0200

    .

--- src/Recognizer.kt
+++ src/Recognizer.kt
@@ -1,4 +1,6 @@
 object Recognizer {
+    val variables = hashMapOf<String, Int>();
+
     fun tokenize(input: String): List<String> {
         val result = mutableListOf<String>()
         var brackets = 0
@@ -37,11 +39,60 @@ object Recognizer {
                 && tokens.zip(funs).all { (t, f) -> f(t) })
     }
 
+    fun term(token: String): Int {
+        val tokens = tokenize(token.removeSurrounding("(", ")"))
+        if (isAdd(token))
+            return term(tokens[1]) + term(tokens[2])
+        if (isSub(token))
+            return term(tokens[1]) - term(tokens[2])
+        if (isMul(token))
+            return term(tokens[1]) * term(tokens[2])
+        if (isVariable(token))
+            return variables.getOrDefault(token, 0)
+        return token.toInt()
+    }
+
+    fun start(token: String) {
+        befehle(token.removeSurrounding("(", ")"))
+        if (variables.containsKey("result"))
+            println(variables["result"])
+    }
+
+    fun befehle(token: String) {
+        tokenize(token)
+            .forEach { t -> befehl(t.removeSurrounding("(", ")")) }
+    }
+
+    fun befehl(token: String) {
+        if (isSet(token))
+            pSet(token) else
+            if (isIf(token))
+                pIf(token) else
+                if (isWhile(token))
+                    pWhile(token)
+    }
+
+    fun pSet(token: String) {
+        val tokens = tokenize(token)
+        variables[tokens[1]] = term(tokens[2])
+    }
+
+    fun pIf(token: String) {
+        val tokens = tokenize(token)
+        if (term(tokens[1]) > 0) start(tokens[2])
+        else start(tokens[3])
+    }
+
+    fun pWhile(token: String) {
+        val tokens = tokenize(token)
+        while (term(tokens[1]) > 0)
+            start(tokens[2])
+    }
+
     fun isArithmetic(token: String, operation: String): Boolean =
         bracketed(token)
         { isOperation(it, { it == operation }, ::isTerm, ::isTerm) }
 
-    // Actual productions
     fun isTerm(token: String): Boolean = listOf(::isVariable, ::isNumber, ::isAdd, ::isSub, ::isMul).any { it(token) }
 
     fun isVariable(token: String) = token.all { it.toInt() in IntRange(0x61, 0x7a) }
@@ -51,20 +102,17 @@ object Recognizer {
     fun isSub(token: String) = isArithmetic(token, "sub")
     fun isMul(token: String) = isArithmetic(token, "mul")
 
-    fun isStmtOuter(token: String): Boolean = bracketed(token, ::isStmtInner)
+    fun isBefehle(token: String): Boolean = bracketed(token, ::isBefehl)
 
-    fun isStmtInner(token: String): Boolean = tokenize(token).all { isStmtOuter(it) } ||
+    fun isBefehl(token: String): Boolean = tokenize(token).all { isBefehle(it) } ||
             listOf(::isIf, ::isWhile, ::isSet).any { it(token) }
 
     fun isIf(token: String): Boolean =
-        bracketed(token)
-        { isOperation(it, { op -> op == "if" }, ::isTerm, ::isStmtOuter, ::isStmtOuter) }
+        isOperation(token, { op -> op == "if" }, ::isTerm, ::isBefehle, ::isBefehle)
 
     fun isWhile(token: String): Boolean =
-        bracketed(token)
-        { isOperation(it, { op -> op == "while" }, ::isTerm, ::isStmtOuter) }
+        isOperation(token, { op -> op == "while" }, ::isTerm, ::isBefehle)
 
     fun isSet(token: String): Boolean =
-        bracketed(token)
-        { isOperation(it, { op -> op == "set" }, ::isVariable, ::isTerm) }
+        isOperation(token, { op -> op == "set" }, ::isVariable, ::isTerm)
 }
--- src/RecognizerTest.kt
+++ src/RecognizerTest.kt
@@ -1,11 +1,6 @@
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.Files.isReadable
-import java.nio.file.Files.walk
-import java.nio.file.Path
-import java.util.stream.Stream
 
 internal class RecognizerTest {
 
@@ -38,17 +33,63 @@ internal class RecognizerTest {
 
     @Test
     fun isStmtO0() {
-        assertTrue(Recognizer.isStmtOuter("(set result (sub x 2021))"))
+        assertTrue(Recognizer.isBefehle("(set result (sub x 2021))"))
     }
 
     @Test
     fun isStmtO1() {
-        assertTrue(Recognizer.isStmtOuter("(set x 42)"))
+        assertTrue(Recognizer.isBefehle("(set x 42)"))
     }
 
     @Test
     fun isStmtO2() {
-        assertTrue(Recognizer.isStmtOuter("((set x 42)(set result (sub x 2021)))"))
+        assertTrue(Recognizer.isBefehle("((set x 42)(set result (sub x 2021)))"))
+    }
+
+    @Nested
+    class parser {
+        @Nested
+        class arithmetic {
+            @Test
+            fun arithmeticAdd() {
+                val act = Recognizer.term("(add 1 2)")
+                assertEquals(3, act)
+            }
+
+            @Test
+            fun arithmeticSub() {
+                val act = Recognizer.term("(sub 5 2)")
+                assertEquals(3, act)
+            }
+
+            @Test
+            fun arithmeticMul() {
+                val act = Recognizer.term("(mul 3 2)")
+                assertEquals(6, act)
+            }
+        }
+
+        @Test
+        fun a1() =
+            Recognizer.start("((set x 42) (set result (sub x 2021)))")
+
+        @Test
+        fun a2() =
+            Recognizer.start(
+                "((set x 1) (while (sub 16 i) ((set x (mul x 2)) (set i (add i 1)))) (set result x))"
+            )
+
+        @Test
+        fun a3() =
+            Recognizer.start(
+                "((set a 247) (set b 299) (while a ((set b (sub b a)) (if (sub a b) ((set t a) (set a b) (set b t)) ()))) (set result b))"
+            )
+
+        @Test
+        fun c2() =
+            Recognizer.start(
+                "((set n 339) (while (sub 1 result) ((set nn (sub n 1)) (while (sub 1 flag) ((set dd 0) (while (sub nn 1) ((set dd (add dd 1)) (set nn (sub nn 2)))) (if nn ((set flag 1)) ((set nn dd) (set d dd) (set r (add r 1)))))) (set k 4) (while k ((set a (mul (add a 1) 89)) (while (sub a 112) ((set a (sub a 113)))) (set x 1) (set dd d) (while dd ((set x (mul x a)) (while (sub x (sub n 1)) ((set x (sub x n)))) (set dd (sub dd 1)))) (set xx (mul (sub x 1) (sub x (sub n 1)))) (set flag 0) (if (mul xx xx) ((set rr (sub r 1)) (while rr ((set x (mul x x)) (while (sub x (sub n 1)) ((set x (sub x n)))) (if (sub (sub n 1) x) () ((set flag 1) (set rr 0))) (set rr (sub rr 1))))) ((set flag 1))) (if (sub 1 flag) ((set k 0)) ()) (set k (sub k 1)))) (if (add k 1) ((set result n)) ((set n (add n 2)))))))"
+            )
     }
 
     @Nested
@@ -61,7 +102,7 @@ internal class RecognizerTest {
                 .map { path ->
                     DynamicTest.dynamicTest(
                         path.toString()
-                    ) { assertTrue(RecognizerM.isProgram(path.readText())) }
+                    ) { Recognizer.befehle(path.readText()) }
                 }
                 .toList()
 
@@ -73,14 +114,14 @@ internal class RecognizerTest {
                 .map { path ->
                     DynamicTest.dynamicTest(
                         path.toString()
-                    ) { assertFalse(RecognizerM.isProgram(path.readText())) }
+                    ) { assertFalse(Recognizer.isBefehle(path.readText())) }
                 }
                 .toList()
 
         @Test
         fun marmortafel() {
             assertFalse(
-                RecognizerM.isProgram(
+                Recognizer.isBefehle(
                     (File("./src/data/invalid/c1").readText())
                 )
             )
